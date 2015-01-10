a2ce0efd9b9fee7aa079623a461ddd9eafdeb151
Kevin Menard <kevin@nirvdrum.com>
2015-01-09 19:30:34 -0500

    [Truffle] Range includes Enumerable.

--- core/src/main/java/org/jruby/truffle/runtime/core/CoreLibrary.java
+++ core/src/main/java/org/jruby/truffle/runtime/core/CoreLibrary.java
@@ -293,6 +293,7 @@ public class CoreLibrary {
         objectClass.include(null, kernelModule);
         numericClass.include(null, comparableModule);
         arrayClass.include(null, enumerableModule);
+        rangeClass.include(null, enumerableModule);
 
         // Set constants
 
